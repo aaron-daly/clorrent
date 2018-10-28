5f47742f1848987d2ec883f617086481b8be3a77
aaron-daly <aaron.daly@outlook.com>
2018-10-28 10:19:37 +0000

    Extract magnet link from torrent file

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject clorrent "0.1.0-SNAPSHOT"
   :description "A simple Clojure torrent client."
   :url "git@github.com:aaron-daly/clorrent.git"
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [bencode "0.2.5"]]
   :main ^:skip-aot clorrent.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
